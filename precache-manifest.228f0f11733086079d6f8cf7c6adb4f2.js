61b0de7744e27dd9e398694a6c6b11e7ef97dca1
Nakul <nakulgopal13199@gmail.com>
2021-06-21 17:08:18 +0530

    Updates

---
+++ precache-manifest.228f0f11733086079d6f8cf7c6adb4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0344e0bf8d9e8a8f420429dd01c79343",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "313b6d37904ad9843568",
+    "url": "/Quiz/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/Quiz/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313b6d37904ad9843568",
+    "url": "/Quiz/static/js/main.2f7f28d7.chunk.js"
+  },
+  {
+    "revision": "30a81f245c0371e34850",
+    "url": "/Quiz/static/js/runtime-main.901e0ade.js"
+  }
+]);
